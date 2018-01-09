191704ec29034870cef85aa195f78e5a3b08fd23
heinwinnaing <heinwinnaing@gmail.com>
2018-01-09 14:01:52 +0630

    -

--- SuperConfigure.Utilities/SuperConfigure.Test/Program.cs
+++ SuperConfigure.Utilities/SuperConfigure.Test/Program.cs
@@ -15,7 +15,6 @@ namespace SuperConfigure.Test
         {
             try
             {
-                Console.WriteLine("starting ...");
                 command.Connection = new System.Data.SqlClient.SqlConnection("Server=USER\\MSSQL2012;Database=shwecrm;User Id=sa;Password=sa;");
                 if (command.ExecuteBoolen("SELECT * FROM [shweaccount]", CommandType.Text, null))
                 {
